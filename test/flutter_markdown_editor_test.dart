6258a93e8aab1a3d9464441985b4a30d7c8b86c6
Ammar Hussein <ammar.atef45@gmail.com>
2020-10-17 22:05:37 -0500

    #1 added a simple test to check codemagic

--- test/flutter_markdown_editor_test.dart
+++ test/flutter_markdown_editor_test.dart
@@ -1,1 +1,26 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown_editor/flutter_markdown_editor.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('create widget', (WidgetTester tester) async {
+    MarkDownEditor editor = MarkDownEditor();
+    await tester.pumpWidget(MyWidget(
+      child: editor.preview,
+    ));
+  });
+}
+
+class MyWidget extends StatelessWidget {
+  final Widget child;
+
+  const MyWidget({Key key, this.child}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: child,
+      ),
+    );
+  }
+}
