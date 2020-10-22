6b74de8d68679dc8b8b4a3d89d089327432c5d10
Ammar Hussein <ammar.atef45@gmail.com>
2020-10-22 10:11:20 -0500

    added a prompt to get value for url and image source

--- lib/editor_icons.dart
+++ lib/editor_icons.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
 
+typedef _prompt = Future<String> Function();
+
+/// A future function that can be used to prompt for a url or image source.
+///
+/// You can use this to show your own dialog and return the prompt value.
+///
+/// Check the function _takeInput for an example.
+typedef UrlSource = Future<String> Function({
+  @required BuildContext context,
+  @required String hint,
+  @required String label,
+});
+
 /// A row of icons(buttons) that can be used to manipulate the text.
 ///
 /// It manipulates the text inside the [controller].
 ///
 /// The function [afterEditing] will be called after the text is edited.
+///
+/// The [urlSource] is afuture function that can be used to prompt
+/// for a url or image source, You can use it to show your
+/// own dialog and return the prompt value, Check the
+/// function _takeInput for an example.
 class MarkdownEditorIcons extends StatelessWidget {
-  /// Create a [MarkdownEditorIcons] passing a [controller] and [afterEditing].
+  /// Create a [MarkdownEditorIcons] passing a [controller],
+  /// [urlSource] and [afterEditing].
   MarkdownEditorIcons({
     Key key,
     @required this.controller,
     this.afterEditing,
+    this.urlSource,
   })  : assert(controller != null),
         super(key: key);
 
@@ -21,6 +41,13 @@ class MarkdownEditorIcons extends StatelessWidget {
 
   /// Will be invoked afer editing the text.
   final Function afterEditing;
+
+  /// A future function that can be used to prompt for a url or image source.
+  ///
+  /// You can use this to show your own dialog and return the prompt value.
+  ///
+  /// Check the function _takeInput for an example.
+  final UrlSource urlSource;
 
   @override
   Widget build(BuildContext context) {
@@ -66,19 +93,32 @@ class MarkdownEditorIcons extends StatelessWidget {
               ),
             ),
             IconButton(
-              tooltip: 'Link',
-              icon: Icon(Icons.link_sharp),
-              onPressed: () => _surroundTextSelection(
-                '[',
-                '](https://)',
-              ),
-            ),
+                tooltip: 'Link',
+                icon: Icon(Icons.link_sharp),
+                onPressed: () {
+                  _surroundTextSelection(
+                    '[',
+                    '](',
+                    prompt: _getPromptFunction(
+                      context: context,
+                      hint: 'https://',
+                      label: 'URL',
+                    ),
+                    afterPrompt: ')',
+                  );
+                }),
             IconButton(
               tooltip: 'Image',
               icon: Icon(Icons.image),
               onPressed: () => _surroundTextSelection(
                 '![',
-                '](https://)',
+                '](',
+                prompt: _getPromptFunction(
+                  context: context,
+                  hint: 'https://',
+                  label: 'Source',
+                ),
+                afterPrompt: ')',
               ),
             ),
           ],
@@ -87,13 +127,37 @@ class MarkdownEditorIcons extends StatelessWidget {
     );
   }
 
-  void _surroundTextSelection(String left, String right) {
+  _prompt _getPromptFunction({
+    String label,
+    String hint,
+    BuildContext context,
+  }) {
+    return () {
+      return urlSource == null
+          ? _takeInput(context: context, hint: hint, label: label)
+          : urlSource(context: context, hint: hint, label: label);
+    };
+  }
+
+  void _surroundTextSelection(
+    String left,
+    String right, {
+    _prompt prompt,
+    String afterPrompt,
+  }) async {
     final currentTextValue = controller.value.text;
     final selection = controller.selection;
     final middle = selection.textInside(currentTextValue);
-    final newTextValue = selection.textBefore(currentTextValue) +
-        '$left$middle$right' +
-        selection.textAfter(currentTextValue);
+    final textBefore = selection.textBefore(currentTextValue);
+    final textAfter = selection.textAfter(currentTextValue);
+    String insertion = '$left$middle$right';
+    if (prompt != null) {
+      assert(afterPrompt != null);
+      insertion += await prompt();
+      insertion += afterPrompt;
+    }
+
+    final newTextValue = '$textBefore$insertion$textAfter';
 
     controller.value = controller.value.copyWith(
       text: newTextValue,
@@ -102,5 +166,61 @@ class MarkdownEditorIcons extends StatelessWidget {
       ),
     );
     if (afterEditing != null) afterEditing();
+  }
+
+  Future<String> _takeInput({
+    @required BuildContext context,
+    @required String hint,
+    @required String label,
+  }) {
+    return showDialog<String>(
+      context: context,
+      builder: (context) {
+        return _InputPrompt(
+          title: 'Enter the value!',
+          hint: hint,
+          label: label,
+        );
+      },
+    );
+  }
+}
+
+class _InputPrompt extends StatelessWidget {
+  final String title;
+  final String label;
+  final String hint;
+
+  final _controller = TextEditingController();
+
+  _InputPrompt({
+    Key key,
+    @required this.title,
+    @required this.label,
+    @required this.hint,
+  })  : assert(title != null),
+        assert(hint != null),
+        super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      title: Text(title),
+      children: [
+        TextFormField(
+          controller: _controller,
+          decoration: InputDecoration(
+            labelText: label,
+            hintText: hint,
+          ),
+        ),
+        RaisedButton.icon(
+          onPressed: () {
+            Navigator.pop<String>(context, _controller.text);
+          },
+          icon: Icon(Icons.done),
+          label: Text('Done'),
+        ),
+      ],
+    );
   }
 }
